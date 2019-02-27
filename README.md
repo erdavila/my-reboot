48d67d314152918852e0193888d8764df6ae4e12
Eduardo R. D'Avila <erdavila@gmail.com>
2019-02-27 19:07:15 -0300

    Implement shutdown and reboot on Linux

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-See https://github.com/scalafx/scalafx-hello-world/issues/6
+[Dependency on Linux](https://github.com/scalafx/scalafx-hello-world/issues/6)
+
+[Linux shutdown/reboot without root](https://askubuntu.com/questions/168879/shutdown-from-terminal-without-entering-password)
--- src/main/scala/myreboot/LinuxPlatform.scala
+++ src/main/scala/myreboot/LinuxPlatform.scala
@@ -6,9 +6,21 @@ class LinuxPlatform extends Platform {
 
   override val actions: Seq[Action] =
     Seq(
-      Action("Desligar") {},
-      Action("Reiniciar") {},
+      Action("Desligar") { shutdown() },
+      Action("Reiniciar") { reboot() },
       Action("Reiniciar no Windows usando o monitor") {},
       Action("Reiniciar no Windows usando a TV") {},
     )
+
+  private def shutdown(): Unit =
+    execute("systemctl", "poweroff")
+
+  private def reboot(): Unit =
+    execute("systemctl", "reboot")
+
+  private def execute(parts: String*): Unit = {
+    val pb = new ProcessBuilder(parts: _*)
+    val p = pb.start()
+    p.waitFor()
+  }
 }
