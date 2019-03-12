931c4cea6c813c653c996aa40e895132ce1374f4
Eduardo R. D'Avila <erdavila@gmail.com>
2019-03-11 23:28:04 -0300

    linux/Setup: fix Grub entries detection

--- linux/src/main/scala/myreboot/main/Setup.scala
+++ linux/src/main/scala/myreboot/main/Setup.scala
@@ -10,14 +10,15 @@ object Setup extends SetupBase {
   def main(args: Array[String]): Unit = {
     val grubCfgFile = new File("/boot/grub/grub.cfg")
     println(s"Lendo ${grubCfgFile.getPath}...")
-    val menuentryLines = Source.fromFile(grubCfgFile).getLines().filter(_ startsWith "menuentry ").toList
+    val menuentryLines = Source.fromFile(grubCfgFile).getLines().filter { line =>
+      (line startsWith "menuentry ") || (line startsWith "submenu ")
+    }.toList
 
     val entries = for (os <- OS.Values)
       yield {
         println("Detectando entrada com \"" + os.code + "\"...")
-        val entryIndex = menuentryLines.indexWhere(_ contains os.code)
-        assert(entryIndex >= 0, "Entrada não encontrada!")
-        val entryNumber = 1 + entryIndex
+        val entryNumber = menuentryLines.indexWhere(_ contains os.code)
+        assert(entryNumber >= 0, "Entrada não encontrada!")
         val key = os.code + "." + Configs.GrubEntrySubKey
         key -> entryNumber.toString
       }
