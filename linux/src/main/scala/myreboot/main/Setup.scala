7914f068d336ecae5b69bc27283cf70d098d45af
Eduardo R. D'Avila <erdavila@gmail.com>
2019-03-25 21:05:12 -0300

    linux/Setup: Use Grub entry IDs instead of positions

--- linux/src/main/scala/myreboot/main/Setup.scala
+++ linux/src/main/scala/myreboot/main/Setup.scala
@@ -7,21 +7,25 @@ import scala.io.Source
 
 object Setup extends SetupBase {
 
+  private val MenuEntryRegex = """.*'([a-zA-Z0-9_\-]+)'\s*\{.*""".r
+
   def main(args: Array[String]): Unit = {
     val grubCfgFile = new File("/boot/grub/grub.cfg")
     println(s"Lendo ${grubCfgFile.getPath}...")
-    val menuentryLines = Source.fromFile(grubCfgFile).getLines().filter { line =>
-      (line startsWith "menuentry ") || (line startsWith "submenu ")
-    }.toList
+    val menuentryLines = Source.fromFile(grubCfgFile).getLines().filter { line => line startsWith "menuentry " }.toList
 
-    val entries = for (os <- OS.Values)
-      yield {
-        println("Detectando entrada com \"" + os.code + "\"...")
-        val entryNumber = menuentryLines.indexWhere(_ contains os.code)
-        assert(entryNumber >= 0, "Entrada não encontrada!")
-        val key = os.code + "." + Configs.GrubEntrySubKey
-        key -> entryNumber.toString
-      }
+    val entries = for {
+      os <- OS.Values
+
+      _ = println("Detectando entrada com \"" + os.code + "\"...")
+      line = menuentryLines.find(_ contains os.code) getOrElse { throw new Error("Entrada não encontrada!") }
+      entryId = line match { case MenuEntryRegex(id) => id }
+
+      key = os.code + "." + Configs.GrubEntrySubKey
+    } yield {
+      key -> entryId
+    }
+
     saveConfigs(entries, Platform.StateDir)
   }
 }
