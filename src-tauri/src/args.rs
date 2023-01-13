9681cb6e7c5274743f5f93447dc5a7c8bf70ccbc
Eduardo R. D'Avila <erdavila@gmail.com>
2023-01-13 00:08:35 -0300

    Implement initial arguments parsing

---
+++ src-tauri/src/args.rs
@@ -0,0 +1,49 @@
+use std::env;
+use std::error::Error;
+use std::fmt::Display;
+
+pub enum ParsedArgs {
+    None,
+    Usage,
+}
+
+#[derive(Debug)]
+pub struct ArgError {
+    message: String,
+    arg: String,
+}
+
+impl ArgError {
+    fn new(message: &str, arg: String) -> ArgError {
+        ArgError {
+            message: message.to_string(),
+            arg,
+        }
+    }
+}
+impl Error for ArgError {}
+impl Display for ArgError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}: {}", self.message, self.arg)
+    }
+}
+
+pub fn parse() -> Result<ParsedArgs, ArgError> {
+    let mut args = env::args();
+    args.next();
+
+    let parsed_args = match args.next() {
+        Some(arg) => match &arg[..] {
+            "-h" | "--help" => ParsedArgs::Usage,
+            _ => return Err(ArgError::new("Argumento inesperado", arg)),
+        },
+        None => ParsedArgs::None,
+    };
+
+    Ok(parsed_args)
+}
+
+pub const USAGE: &str = "\
+Usos:
+  my-reboot -h|--help
+    Exibe este conteúdo.";
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,13 @@
     windows_subsystem = "windows"
 )]
 
+mod args;
 mod host_os;
+
+use std::env;
+use std::error::Error;
+
+use crate::args::ParsedArgs;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -11,11 +17,18 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn main() {
-    host_os::enumerate_display_devices();
+fn main() -> Result<(), Box<dyn Error>> {
+    match args::parse()? {
+        ParsedArgs::None => {
+            host_os::enumerate_display_devices();
 
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+            tauri::Builder::default()
+                .invoke_handler(tauri::generate_handler![greet])
+                .run(tauri::generate_context!())
+                .expect("error while running tauri application");
+        }
+        ParsedArgs::Usage => println!("{}", args::USAGE),
+    }
+
+    Ok(())
 }
