bdbb8a8bc753b10689f1cf60b92f78e1ab6fa0fd
Eduardo D'Avila <edavila@gmail.com>
2026-06-09 17:17:43 -0300

    Use cargo to install the binary

--- install.ps1
+++ install.ps1
@@ -5,42 +5,33 @@
 $ErrorActionPreference = "Stop"
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
-$ScriptDir = $PSScriptRoot
-if (-not $ScriptDir) { $ScriptDir = Get-Location }
-
 $ExeName = "my-reboot.exe"
-$DestDir = Join-Path $env:LOCALAPPDATA "Programs\my-reboot"
+$AppName = "My Reboot"
 $SwitchProfileTitle = "Trocar Perfil"
 
 function Initialize-Executable {
     Write-Host "* Compilando..."
-    cargo build --release --config "env.WINDOWS_ICON='true'"
+    cargo install --path . --config "env.WINDOWS_ICON='true'"
     Write-Host ""
 
-    Write-Host "* Copiando $ExeName para $DestDir ..."
-    if (-not (Test-Path $DestDir)) {
-        New-Item -ItemType Directory -Force -Path $DestDir | Out-Null
-    }
-    Copy-Item (Join-Path $ScriptDir "target\release\$ExeName") "$DestDir"
-    Write-Host ""
-
-    return Join-Path $DestDir $ExeName
+    return Join-Path $env:USERPROFILE ".cargo\bin\$ExeName"
 }
 
 function New-Shortcut {
     param(
         [Parameter(Mandatory = $true)][string]$title,
-        [Parameter(Mandatory = $true)][string]$exePath,
         [string]$arguments,
         [string]$iconLocation,
         [string]$hotkey
     )
 
+    Write-Host "* Criando atalho para $title na Área de Trabalho..."
+
     $WshShell = New-Object -ComObject WScript.Shell
     $DesktopPath = [System.Environment]::GetFolderPath("Desktop")
 
     $Shortcut = $WshShell.CreateShortcut((Join-Path $DesktopPath "$title.lnk"))
-    $Shortcut.TargetPath = $exePath
+    $Shortcut.TargetPath = $ExeName
     if ($PSBoundParameters.ContainsKey("arguments")) {
         $Shortcut.Arguments = $arguments
     }
@@ -56,11 +47,9 @@ function New-Shortcut {
     $Shortcut.Save()
 }
 
-function Install-Shortcuts($exePath) {
-    Write-Host "* Criando atalho para My Reboot na Área de Trabalho..."
+function Install-Shortcuts {
     $mainShortcutParams = @{
-        title = "My Reboot"
-        exePath = $exePath
+        title = $AppName
     }
     New-Shortcut @mainShortcutParams
 
@@ -68,11 +57,8 @@ function Install-Shortcuts($exePath) {
     if (-not $displaySwitchPath) {
         $displaySwitchPath = "C:\Windows\System32\DisplaySwitch.exe"
     }
-
-    Write-Host "* Criando atalho para $SwitchProfileTitle na Área de Trabalho..."
     $profileSwitchShortcutParams = @{
         title = $SwitchProfileTitle
-        exePath = $exePath
         arguments = "switch:other"
         iconLocation = "$displaySwitchPath,2"
         hotkey = "ALT+CTRL+X"
@@ -83,7 +69,7 @@ function Install-Shortcuts($exePath) {
 function Set-ProfileSwitchRunOnStartup($exePath) {
     Write-Host "* Registrando troca de perfil ao iniciar o Windows..."
     $registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
-    $name = "My Reboot Profile Switch"
+    $name = "$AppName Profile Switch"
     $value = "`"$exePath`" switch:saved"
 
     if (-not (Test-Path $registryPath)) {
@@ -92,25 +78,13 @@ function Set-ProfileSwitchRunOnStartup($exePath) {
     Set-ItemProperty -Path $registryPath -Name $name -Value $value
 }
 
-function Add-ToPath($dir) {
-    Write-Host "* Verificando PATH..."
-    $path = [Environment]::GetEnvironmentVariable("PATH", "User")
-    $dirs = $path -split ";"
-    if ($dirs -notcontains $dir) {
-        Write-Host "* Adicionando $dir ao PATH do usuário..."
-        $newPath = "$path;$dir"
-        [Environment]::SetEnvironmentVariable("PATH", $newPath, "User")
-        $env:PATH += ";$dir"
-    }
-}
-
 $exePath = Initialize-Executable
-Install-Shortcuts $exePath
+Install-Shortcuts
 Set-ProfileSwitchRunOnStartup $exePath
-Add-ToPath $DestDir
 
 Write-Host ""
 Write-Host "* Instalação concluída"
+Write-Host ""
 Write-Host "Execute:"
 Write-Host "  my-reboot configure"
 Write-Host "para configurar."
--- install.sh
+++ install.sh
@@ -2,46 +2,32 @@
 set -e
 
 EXE_NAME=my-reboot
-DEST_DIR=~/Apps/my-reboot
+ICON_NAME=$EXE_NAME
 
 function prepare_executable() {
   echo "* Compilando..."
-  cargo build --release
-  echo
-
-  echo "* Copiando $EXE_NAME para $DEST_DIR ..."
-  mkdir -p "$DEST_DIR"
-  cp ./target/release/"$EXE_NAME" "$DEST_DIR"
-  echo
-
-  EXE_PATH="$DEST_DIR/$EXE_NAME"
-}
-
-function create_link() {
-  DEST_PATH=~/bin
-  echo "* Criando link para $EXE_NAME em $DEST_PATH ..."
-  ln -sf "$EXE_PATH" "$DEST_PATH"
+  cargo install --path .
   echo
 }
 
 function install_launching_icon() {
-  local ICON_FILE='icon-256x256.png'
-  local DESKTOP_FILE_PATH="$DEST_DIR"/my-reboot.desktop
-  echo "* Instalando ícone..."
-  cp assets/"$ICON_FILE" "$DEST_DIR"
+  echo "* Instalando ícone no menu..."
+
+  xdg-icon-resource install --novendor --mode user --size 256 assets/icon-256x256.png $ICON_NAME
+
+  local DESKTOP_FILE_PATH=$(mktemp --directory)/my-reboot.desktop
   echo "[Desktop Entry]
 Version=3.0.0
 Type=Application
 Name=My Reboot
-Icon=$DEST_DIR/$ICON_FILE
-Exec=$EXE_PATH
+Icon=$ICON_NAME
+Exec=$EXE_NAME
 Comment=Reboot options
 Categories=System;Utility;
 Terminal=false
-" >"$DESKTOP_FILE_PATH"
-
+" >$DESKTOP_FILE_PATH
   xdg-desktop-menu install --novendor --mode user $DESKTOP_FILE_PATH
-  xdg-desktop-menu forceupdate --mode user
+
   echo
 }
 
@@ -52,10 +38,10 @@ if [ "$(uname -o)" != "GNU/Linux" ]; then
 fi >&2
 
 prepare_executable
-create_link
 install_launching_icon
 
 echo "* Instalação concluída"
+echo
 echo "Execute:"
 echo "  my-reboot configure"
 echo "para configurar."
